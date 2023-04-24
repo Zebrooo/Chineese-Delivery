0fe3aa0690f5deea9ea3db33ae15984235df2182
Zit8 <v.kaledinceff@gmail.com>
2023-04-24 18:54:16 +0300

    rollback

--- Client/config/build/builResolvers.ts
+++ Client/config/build/builResolvers.ts
@@ -1,12 +1,8 @@
-import { ResolveOptions } from "webpack";
-import { BuildOptions } from "./types/config";
+import { ResolveOptions } from 'webpack';
+import { BuildOptions } from './types/config';
 
-export default function buildResolvers(options: BuildOptions) : ResolveOptions {
+export default function buildResolvers(): ResolveOptions {
   return {
-    extensions: [".tsx", ".ts", ".js"],
-    preferAbsolute: true,
-    modules: [options.paths.src, 'node-modules'],
-    mainFiles: ['index'],
-    alias: {}
+    extensions: ['.tsx', '.ts', '.js'],
   };
 }
--- Client/webpack.config.ts
+++ Client/webpack.config.ts
@@ -1,18 +1,17 @@
-import webpack from "webpack";
-import buildWebpackConfig from "./config/buildWebpackConfig";
-import { BuildEnv, BuildPath } from "./config/build/types/config";
-import path from "path";
+import webpack from 'webpack';
+import buildWebpackConfig from './config/buildWebpackConfig';
+import { BuildEnv, BuildPath } from './config/build/types/config';
+import path from 'path';
 
 export default (env: BuildEnv) => {
   const paths: BuildPath = {
-    entry: path.resolve(__dirname, "src", "index.tsx"),
-    build: path.resolve(__dirname, "build"),
-    html: path.resolve(__dirname, "public", "index.html"),
-    src: path.resolve(__dirname, "src")
+    entry: path.resolve(__dirname, 'src', 'index.tsx'),
+    build: path.resolve(__dirname, 'build'),
+    html: path.resolve(__dirname, 'public', 'index.html'),
   };
 
-  const mode = env.mode || "development";
-  const isDev = mode === "development";
+  const mode = env.mode || 'development';
+  const isDev = mode === 'development';
   const PORT = env.port || 3000;
   const config: webpack.Configuration = buildWebpackConfig({
     mode,
